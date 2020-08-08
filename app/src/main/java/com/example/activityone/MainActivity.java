4d540386bc27ca34e69dfd96f8031084c51463f4
User <chathunikaveesha99@gmail.com>
2020-08-08 14:04:14 +0800

    Second Commit

--- app/src/main/java/com/example/activityone/MainActivity.java
+++ app/src/main/java/com/example/activityone/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.activityone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMsg2;
+    private static final String TAG = "LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvMsg2 = findViewById(R.id.tvMsg2);
-
         tvMsg2.setText(R.string.Msg2);
+
+        Log.i(TAG, "onCreate: ");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+   @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
 }
